cebb1c1e9c69793c0101bc084ace24caa2166f6a
James Irwin <irwineffect@gmail.com>
2016-09-26 23:06:07 -0700

    added more comments to lab2 example

--- nodes/lab2_example.py
+++ nodes/lab2_example.py
@@ -7,11 +7,24 @@ import cv2
 import numpy as np
 from intro_to_robotics.image_converter import ToOpenCV, depthToOpenCV
 
+
+#this function does our image processing
+#returns the location and "size" of the detected object
 def process_image(image):
+    #convert color space from BGR to HSV
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    #create bounds for our color filter
     lower_bound = np.array([0, 10, 10])
     upper_bound = np.array([10,255,255])
+
+    #execute the color filter, returns a binary black/white image
     mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
+
+    #display the results of the color filter
+    cv2.imshow("image_mask", mask)
+
+    #calculate the centroid of the results of the color filer
     M = cv2.moments(mask)
     location = None
     magnitude = 0
@@ -20,11 +33,11 @@ def process_image(image):
         cy = int(M['m01']/M['m00'])
         magnitude = M['m00']
         location = (cx-320, cy-240) #scale so that 0,0 is center of screen
-        #draw a circle image where we detected the midpoint of the object
+        #draw a circle image where we detected the centroid of the object
         cv2.circle(image, (cx,cy), 3, (0,0,255), -1)
 
+    #display the original image with the centroid drawn on the image
     cv2.imshow("processing result", image)
-    cv2.imshow("image_mask", mask)
 
     #waitKey() is necessary for making all the cv2.imshow() commands work
     cv2.waitKey(1)
@@ -36,16 +49,20 @@ class Node:
         #register a subscriber callback that receives images
         self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback, queue_size=1)
 
+        #create a publisher for sending commands to turtlebot
         self.movement_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
 
+    #this function wll get called every time a new image comes in
+    #all logic occurs in this function
     def image_callback(self, ros_image):
         # convert the ros image to a format openCV can use
         cv_image = np.asarray(ToOpenCV(ros_image))
 
         #run our vision processing algorithm to pick out the object
         #returns the location (x,y) of the object on the screen, and the
-        #magnitude of the discovered object. Magnitude can be used to estimate
+        #"size" of the discovered object. Size can be used to estimate
         #distance
+        #None/0 is returned if no object is seen
         location, magnitude = process_image(cv_image)
 
         #log the processing results
